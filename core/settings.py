2f42bae75cb4154dd65df9dde2210e21e4505df8
Alex <a@hobby4me.ru>
2019-10-12 14:20:28 +0300

    Remove non-default settings

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wa#!*+=068&!ft^&d6g5uko6ynlv^42(sl78n#kv2shax$xc27'
+SECRET_KEY = ''
+if not SECRET_KEY:
+    raise NotImplemented('Missing SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
