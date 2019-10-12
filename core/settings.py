e7fea78cf2b23d34fc69823c35a1b2c57cf98eb5
Alex <a@hobby4me.ru>
2019-10-12 22:23:39 +0300

    Add debug_toolbar and SECRET_KEY env var

--- core/settings.py
+++ core/settings.py
@@ -9,9 +9,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-if not SECRET_KEY:
-    raise NotImplemented('Missing SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,10 +30,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
